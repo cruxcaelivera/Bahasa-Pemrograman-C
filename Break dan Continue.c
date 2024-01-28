55b5cab98ffda08088f45460818d822c2daddc56
cruxcaelivera <132868382+cruxcaelivera@users.noreply.github.com>
2024-01-28 23:06:29 +0700

    Update Break dan Continue.c

--- Break dan Continue.c
+++ Break dan Continue.c
@@ -12,3 +12,20 @@ int main()
     num--;
     }
 }
+
+Saat Anda ingin tetap berada di loop, tetapi langsung beralih ke iterasi berikutnya, Anda menggunakan pernyataan continue.
+Sebagai contoh:
+#include <stdio.h>
+int main()
+{
+    int num = 5;
+    while (num > 0) {
+      num--; //Iterasi pertama num = 4
+      if (num == 3)
+          continue; // continue = Keluar dari loop, while lanjut dieksekusi tanpa kondisi yang membuat loop berhenti
+      //num--; num akan selalu = 3 
+      printf("%d\n", num); //Iterasi pertama num = 4
+      //num--; Jika num-- ada pada setelah printf, num akan selalu = 3 ketika mengeksekusi pernyataan continue (Looping tak terbatas dengan output tidak ada)
+    }
+}
+Dalam kode di atas, jika num dikurangi setelah pernyataan continue, loop tak terbatas akan dibuat.
