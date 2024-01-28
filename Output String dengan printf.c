65cfa55ccd610bf4ee0526d77620f60906ce7c89
cruxcaelivera <132868382+cruxcaelivera@users.noreply.github.com>
2024-01-28 22:18:35 +0700

    Create Output String dengan printf.c

---
+++ Output String dengan printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hi, everyone!");
+    return 0;
+}
+atau 
+#include <stdio.h>
+int main() 
+{
+  return "Hi, everyone!"; //Tidak bisa mengembalikan string menggunakan return
+}
+atau
+#include <stdio.h>
+int main()
+{
+  return 20; //Tidak menghasilkan keluaran 
+}
