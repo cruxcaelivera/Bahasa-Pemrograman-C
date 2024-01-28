76a2913e370930219346cfa05c5c52903892792d
cruxcaelivera <132868382+cruxcaelivera@users.noreply.github.com>
2024-01-28 22:17:00 +0700

    Create Hello World.c

---
+++ Hello World.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //Fungsi yang digunakan untuk menghasilkan keluaran didefinisikan dalam stdio.h 
+int main() //Titik masuk ke suatu program / Yang pertama dieksekusi oleh program
+{ //Tanda kurung kurawal menunjukkan awal 
+    printf("Hello World\n"); //Newline menampilkan karakter baris baru
+    return 0; //Mengembalikan nilai 0 ke proses pemanggilan, 0 umumnya berarti program berhasil dieksekusi dan angka lain menunjukkan bahwa program telah gagal. 
+    //Titik koma ; menunjukkan akhir pernyataan 
+} //Menunjukkan akhir fungsi 
